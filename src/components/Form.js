cd84350916f43c0211e405dac79a8da30cf38344
tamara-munoz <tami.skt@gmail.com>
2020-09-17 19:02:44 -0300

    feat: catching the info inside the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+
+    state = {
+        title: '',
+        description: ''
+    }
+
+    handleSubmit = (e) => {
+        console.log(this.state)
+        e.preventDefault();
+    }
+
+    handleChange = (e) => {
+        console.log(e.target.name, e.target.value)
+        this.setState({ 
+            [e.target.name]: e.target.value 
+        })
+    }
+
     render() {
+
         return (
             <div>
-                <form>
-                    <input type="text" placeholder="Write a task" />
+                <form onSubmit={this.handleSubmit}>
+                    <input 
+                        type="text"
+                        name="title"
+                        placeholder="Write a task" 
+                        onChange={this.handleChange} 
+                        value={this.state.title} 
+                    />
+                    <br/>
+                    <textarea 
+                        cols="30" 
+                        rows="10"
+                        name="description"
+                        placeholder="Write a description" 
+                        onChange={this.handleChange} 
+                        value={this.state.description}
+                    ></textarea>
+                    <br/>
+                    <button type="submit">Save task</button>
                 </form>
             </div>
         );
