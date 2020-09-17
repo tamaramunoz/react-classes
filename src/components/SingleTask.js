b0e3d18657dfb2b6b43cd28fde6c580bc2c4116a
tamara-munoz <tami.skt@gmail.com>
2020-09-17 16:42:10 -0300

    feat: styled component

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 
+
 class SingleTask extends Component {
     render() {
 
         const { item } = this.props; //calling the props directly instead put this.props.item.title
-
 
         return (
             <div>
                 <h5>{item.title}</h5>
                 <p>{item.description}</p>
                 <input type="checkbox" />
-                <button>
+                <button style={btnDelete}>
                     x
                 </button>
             </div>
         );
     }
+}
+
+const btnDelete = {
+    frontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    padding: '10px 15px',
+    borderRadius: '50%',
+    cursor: 'pointer',
 }
 
 export default SingleTask;
